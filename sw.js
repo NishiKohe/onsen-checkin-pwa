dc678b5b76de13b118b7a6de6c77b4aa6e5046b8
NishiKohe <154313496+NishiKohe@users.noreply.github.com>
2026-08-23 20:44:02 +0900

    Bump PWA cache for Kinki national recreation data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "onsen-checkin-v19";
+const CACHE_NAME = "onsen-checkin-v20";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -21,9 +21,11 @@ const APP_ASSETS = [
   "./data/national-recreation-kanto.json",
   "./data/national-recreation-koshinetsu-hokuriku.json",
   "./data/national-recreation-tokai.json",
+  "./data/national-recreation-kinki.json",
   "./data/national-recreation-overrides.json",
   "./data/national-recreation-overrides-koshinetsu-hokuriku.json",
   "./data/national-recreation-overrides-tokai.json",
+  "./data/national-recreation-overrides-kinki.json",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
