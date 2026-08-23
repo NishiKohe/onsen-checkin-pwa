216e484340648587d0827e0c54b14d967286d244
NishiKohe <154313496+NishiKohe@users.noreply.github.com>
2026-08-23 19:01:56 +0900

    Bump PWA cache for Koshinetsu Hokuriku data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "onsen-checkin-v7";
+const CACHE_NAME = "onsen-checkin-v8";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -9,6 +9,7 @@ const APP_ASSETS = [
   "./data/onsen-musume-hokkaido-tohoku.json",
   "./data/onsen-musume-kanto-simple.json",
   "./data/onsen-musume-kanto-areas.json",
+  "./data/onsen-musume-koshinetsu-hokuriku-simple.json",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
