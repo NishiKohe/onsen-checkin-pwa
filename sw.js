88e5e985e9bbac161b87d393a7a306ffd4219a93
NishiKohe <154313496+NishiKohe@users.noreply.github.com>
2026-08-31 22:28:28 +0900

    v70.5: rotate cache for achievement footer fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "onsen-checkin-v70.4";
+const CACHE_NAME = "onsen-checkin-v70.5";
 const CORE_ASSETS = [
   "./", "./index.html", "./manifest.webmanifest", "./style.css", "./app-shell.css",
   "./game-ui-v60.css", "./game-hub-v61.css", "./encyclopedia-ui-v61.css", "./mining-game-v69.css", "./mining-pickaxe-v691.css",
